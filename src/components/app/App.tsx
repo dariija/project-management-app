586a9c1c74ad11522f79310de749d60369add133
Daria Bakanova <darija.bakanova@gmail.com>
2022-05-08 19:38:30 +0300

    feat: add welcome page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import Footer from '../footer/Footer';
+import HeaderContainer from '../header/HeaderContainer';
+import Main from '../main/Main';
+import WelcomePage from '../pages/welcome/WelcomePage';
 import styles from './App.module.css';
 
 function App() {
-  return <></>;
+  return (
+    <div className={styles.app_wrapper}>
+      <HeaderContainer />
+      <Main>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+        </Routes>
+      </Main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/pages/welcome/WelcomePage.tsx
@@ -0,0 +1,81 @@
+import styles from './WelcomePage.module.css';
+
+export default function WelcomePage() {
+  return (
+    <>
+      <section className={styles.section}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quis consequatur qui,
+        repellendus et sequi ipsa in adipisci asperiores ullam, saepe enim velit quidem sunt neque,
+        libero eius nostrum iure!
+      </section>
+      <section className={styles.section}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit similique, nisi natus iste
+        vero provident quidem rem voluptatibus, tenetur, vel pariatur quia ipsam? Possimus, impedit
+        laboriosam numquam corporis blanditiis quam. Veniam quam totam, accusamus officiis explicabo
+        cumque dolorum qui placeat consequatur, itaque corporis quas sint eius praesentium alias
+        numquam magni architecto ipsum dicta a deleniti saepe consequuntur eaque. Magnam, officia.
+        Reprehenderit corrupti repellendus culpa magni ducimus? Doloremque dolorem provident natus,
+        beatae veniam id ab voluptates, accusantium amet odio culpa, laudantium vero ipsum. Deserunt
+        a expedita et nobis? Voluptates, sapiente praesentium. Pariatur sit, maiores doloremque
+        voluptatem voluptas adipisci atque. Unde, praesentium deserunt inventore eum explicabo
+        beatae! Ipsa alias, consequuntur aut, vel, in adipisci nostrum vitae voluptatibus soluta
+        ipsum rerum est animi. Doloribus suscipit possimus perferendis ullam, facilis facere nemo
+        mollitia quas repudiandae modi nobis excepturi delectus reiciendis sed voluptatum asperiores
+        sequi. Ratione, ipsam sit cumque neque repellat molestias repudiandae magnam similique?
+        Perspiciatis quis expedita molestias, atque cupiditate omnis voluptates consectetur dolorum
+        repellendus reprehenderit qui et incidunt natus nostrum commodi dolores iure impedit rem
+        laboriosam explicabo cumque facilis, eius, temporibus ad. Non. Suscipit odit saepe eos nihil
+        quas voluptate rem, consequuntur harum consectetur, reiciendis doloribus expedita cumque
+        ipsum obcaecati! Nostrum, officiis libero totam ipsum qui non vel consectetur possimus
+        numquam voluptas esse. Cumque nisi corrupti vero voluptatibus perferendis. Doloribus
+        temporibus itaque in necessitatibus, id suscipit cupiditate illo possimus totam deserunt
+        dolor neque facere earum similique aut qui voluptatibus quod. Voluptatum, minima voluptatem.
+        Beatae veniam impedit architecto ipsum consequatur odio voluptatibus delectus fugit,
+        provident voluptatum quas aspernatur. Vitae, sapiente quas velit quod assumenda impedit
+        provident, debitis saepe eaque voluptate odio, corrupti eos nemo. Sed ducimus quod
+        reiciendis repellendus assumenda voluptatem autem, vel quae quos alias corporis voluptates
+        distinctio expedita architecto magni maxime recusandae possimus atque quas, blanditiis,
+        dolor asperiores? Facilis cupiditate quisquam quasi!
+      </section>
+      <section className={styles.section}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam voluptates voluptatibus
+        dolorem rem, excepturi natus hic. Voluptatum eligendi rem deleniti soluta. Fugiat illum
+        cumque molestias, eveniet quibusdam aspernatur repellat eius. Ab quaerat eos inventore
+        asperiores quibusdam repellendus autem unde maiores nostrum vel porro quae quidem labore
+        aut, magni earum reiciendis veritatis incidunt tempora ullam ut officiis. Id voluptatum
+        aliquam placeat. Temporibus fuga consequatur excepturi placeat consequuntur. Facilis itaque
+        dolorem a, perferendis sunt maiores distinctio asperiores culpa error impedit expedita
+        saepe, ducimus molestias excepturi ea adipisci ullam vitae! Sunt, impedit consequuntur.
+        Labore, omnis sunt! Quibusdam eveniet, impedit adipisci necessitatibus assumenda soluta
+        dolorem debitis ducimus delectus minus numquam quod laboriosam, fugiat tenetur itaque fuga
+        eos sequi reiciendis quis quae animi voluptatem reprehenderit. Itaque debitis nostrum
+        voluptatibus aperiam optio, vero saepe minus animi ipsum ad nihil delectus quo magni fuga
+        deserunt laborum dicta quos? Recusandae veritatis quis sequi tenetur quisquam fuga nesciunt
+        iure! Saepe, voluptatem placeat pariatur voluptas tempore sunt nisi unde ex numquam velit
+        esse non necessitatibus fugiat consequuntur sed nihil ea tenetur reprehenderit rerum, vero
+        vitae animi praesentium laudantium. Autem, porro.
+      </section>
+      <section className={styles.section}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa tempore maiores earum ducimus
+        molestias, fugiat natus molestiae perspiciatis nulla voluptas est. Animi pariatur corporis
+        temporibus odio voluptatem nesciunt eius inventore! Quidem molestias obcaecati amet,
+        praesentium consequuntur reprehenderit quasi laborum deserunt sit maxime quibusdam dolores
+        possimus quae voluptatum placeat. Veniam quae tempora error unde rerum aliquid eveniet in,
+        corrupti animi repudiandae!
+      </section>
+      <section className={styles.section}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad harum reprehenderit et illum
+        laudantium, officia earum at quasi velit tempora odio, quibusdam in quam aspernatur libero
+        fugit quos cupiditate nihil! Mollitia doloribus laudantium nulla quasi sapiente, repudiandae
+        porro illum incidunt alias velit facere quae odit modi quaerat voluptatem fuga reiciendis ex
+        inventore dolore fugit dolores molestias quibusdam unde. Inventore, corrupti? Deleniti enim
+        reiciendis consequuntur consectetur sint explicabo a dolores suscipit molestias modi dolorum
+        exercitationem possimus eveniet, consequatur, labore cupiditate libero? Maiores placeat
+        quasi ad non quae expedita quibusdam, provident deserunt. Rem eveniet quia corporis veniam
+        quisquam, delectus dolor illo? Molestias suscipit eos sequi accusamus ea ut dignissimos
+        incidunt neque, porro iusto magni reprehenderit veritatis officiis ad nesciunt! Sed,
+        temporibus alias?
+      </section>
+    </>
+  );
+}
